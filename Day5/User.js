231241d67a7d68681258d8942bd9569a26146919
samar-programmer <68153834+samar-programmer@users.noreply.github.com>
2020-07-21 00:11:20 -0700

    Add files via upload

--- Day5/User.js
+++ Day5/User.js
@@ -8,14 +8,14 @@ class User{
 
     }
 
-    login(){
-        console.log(`${this.name} has logged in`);
+    login(users){
+        console.log(`${users.name} has logged in`);
         return this;
         
     }
 
-    logout(){
-        console.log(`${this.name} has logged out`);
+    logout(users){
+        console.log(`${users.name} has logged out`);
         return this;
 
     }
@@ -23,39 +23,53 @@ class User{
     
 }
 
+console.log('individual craete user');
 let user1 = new User('sasikumar',22,'programmer7@gmail.com');
-let user2 = new User('sashdfr',22,'programmer7@gmail.com');
-user1.login();
-user1.logout();
+console.log(user1);
+user1.login(user1);
+user1.logout(user1);
 
 class Moderator extends User{
-    constructor(name,age,email,coins){
-        super(name,age,email);
-        this.coins = coins;
+    addCoins(users,coins){
+        users.coins=coins;
+        console.log(`Moderator and admin add ${users.coins}coins to the  ${users.name}`);
+        console.log(users)
     }
-
-    addCoins(){
-        this.coins++;
-        console.log(`${this.name} has ${this.coins} coins`);
-    }
-    delCoins(){
-        this.coins--;
-        console.log(`${this.name} has ${this.coins} coins`);
+    delCoins(users){
+        users.coins=0;
+        console.log(`Moderator and admin delete coins to the  ${users.name}`);
+        console.log(users)
     }
 }
 
-let adc=new Moderator('samar',23,'hgadhag@gmail.com',10);
-adc.addCoins();
-adc.addCoins();
-adc.delCoins();
-adc.delCoins();
+console.log('Moderator can create user')
+let user2=new Moderator('samar',23,'hgadhag@gmail.com');
+let mod=new Moderator();
+mod.login(user2);
+mod.addCoins(user2,50);
+mod.delCoins(user2);
+mod.logout(user2);
+
 
  class Admin extends Moderator{
     addCourse(user,course){
         user.courses.push(course);
         console.log(user);
     }
+    delParticularcourse(user,course){
+        user.courses.filter(el=>el!=course);
+        console.log(`Admin delete Particular Course of ${course}`);
+        console.log(user);
+    }
  }
-
- let admin = new Admin('kings',19,'sjhjl@;.com',10);
- admin.addCourse(user1,'Javascript');
+ console.log('admin create user')
+ let user3 = new Admin('kings',19,'sjhjl@;.com');
+ console.log(user3);
+ ad=new Admin();
+ ad.login(user3);
+ ad.addCoins(user3,20);
+ ad.delCoins(user3);
+ ad.addCourse(user1,'Javascript');
+ ad.addCourse(user1,'python');
+ ad.delParticularcourse(user3,'python');
+ ad.logout(user3);
